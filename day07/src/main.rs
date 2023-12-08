a7a0a5ccb68660985e6f76f4d2d5412aeb61e5b8
RinguPingu <love.arvidsson@gmail.com>
2023-12-08 13:42:19 +0100

    Add

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,46 @@
+#[derive(Debug)]
+enum HandType {
+    FiveKind,
+    FourKind,
+    FullHouse,
+    ThreeKind,
+    TwoPair,
+    OnePair,
+    HighCard,
+}
+
+#[derive(Debug)]
+struct Hand {
+    cards: String,
+    bet: u32,
+    hand_type: Option<HandType>,
+}
+
+impl Hand {
+    fn new(cards: String, bet: u32) -> Self {
+        Hand {
+            cards,
+            bet,
+            hand_type: None,
+        }
+    }
+}
+
 fn main() {
-    let input = std::fs::read_to_string("./src/input/input.txt").expect("Invalid File");
+    let input = std::fs::read_to_string("./src/input/example_input.txt").expect("Invalid File");
+
+    let hands: Vec<Hand> = input
+        .lines()
+        .map(|s| s.split_whitespace().collect::<Vec<&str>>())
+        .map(|v| {
+            Hand::new(
+                v.first().unwrap().to_string(),
+                v.last().unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .collect();
+
+    for hand in hands {
+        println!("{:?}", hand);
+    }
 }
